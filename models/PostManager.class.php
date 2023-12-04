f3b2bdc18c38dc483a1444f69212b74850cf2a7e
thomas-clr-dev <thomas.clarisse@my-digital-school.org>
2023-12-04 16:00:34 +0100

    add gePostFromDB

--- models/PostManager.class.php
+++ models/PostManager.class.php
@@ -1,6 +1,8 @@
 <?php
 
-class PostManager{
+require_once 'models/Model.class.php';
+
+class PostManager extends Model{
 
     private $posts = [];
 
@@ -10,5 +12,15 @@ class PostManager{
 
     public function addPost(Post $post){
         $this->posts[] = $post;
+    }
+
+    public function getPostFromDb(){
+        $req = $this->getBdd()->prepare('SELECT * FROM posts');
+        $req->execute();
+        $posts = $req->fetchAll(PDO::FETCH_ASSOC);
+        print_r($posts);
+        foreach($posts as $post){
+            $this->addPost(new Post($post['id'], $post['header'], $post['title'], $post['author'], $post['image'], $post['content'], $post['date']));
+        }
     }
 }
